fe2ea6d2c1c243679d9c5e9e7de66b3198d4a4ea
from0toweb <t9052585903@gmail.com>
2020-05-04 14:39:49 +0300

    added cssmin and rename plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
-const gulp = require("gulp");
-const browserSync = require('browser-sync').create();
+var gulp = require("gulp"),
+    browserSync = require('browser-sync').create(),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function(){
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('min', function (cb){
+  gulp.src('css/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('css/'))
+      cb()
 });
